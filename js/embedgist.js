88cafcccf2f644124019970cf9f0d853dea6b733
Hans de Rooij <hansderooij@hotmail.com>
2020-09-09 19:43:48 +0200

    Added file embedgist.js
    JavaScript associated with the HTML5 index file

---
+++ js/embedgist.js
@@ -0,0 +1,4 @@
+//JavaScript code to load when the HTML DOM is loaded
+document.addEventListener('DOMContentLoaded', event => {
+   console.log('DOM content loaded, hosted on URL ' + window.location);
+});
