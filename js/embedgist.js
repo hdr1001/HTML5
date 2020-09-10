af8f252a3fc33f46942ee0a35cbe98741c0283af
Hans de Rooij <hansderooij@hotmail.com>
2020-09-10 09:32:34 +0200

    Added code to locate gist placeholders
    Added code to locate gist placeholders on the page

--- js/embedgist.js
+++ js/embedgist.js
@@ -1,4 +1,13 @@
+//Locate the gist placeholders & replace them with the gist
+function addGtiHubGists() {
+   document.querySelectorAll('.ghgist').forEach(elem => {
+      console.log('Located placeholder for gist with id ' + elem.getAttribute('data-ghgistid'));
+   });
+}
+
 //JavaScript code to load when the HTML DOM is loaded
 document.addEventListener('DOMContentLoaded', event => {
    console.log('DOM content loaded, hosted on URL ' + window.location);
+   
+   addGtiHubGists();
 });
