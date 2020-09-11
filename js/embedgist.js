96323d967c7c8ed8f7d2850ea7c0d6be03f1bfa3
Hans de Rooij <HansDeRooij@Hotmail.com>
2020-09-11 17:29:00 +0200

    Added formatting to the GitHub Gist (pt.1)

--- js/embedgist.js
+++ js/embedgist.js
@@ -1,3 +1,63 @@
+//Create the DOM element node for the GitHub Gist
+function getGitHubGistElem(oGist) {
+   let gistTopDiv = document.createElement('div');
+   gistTopDiv.setAttribute('class', 'gist')
+
+   let gistDiv = document.createElement('div');
+   gistDiv.setAttribute('class', 'gist-file');
+
+   let gistBottomDiv = gistTopDiv.appendChild(gistDiv);
+
+   gistDiv = document.createElement('div');
+   gistDiv.setAttribute('class', 'gist-data');
+
+   gistBottomDiv = gistBottomDiv.appendChild(gistDiv);
+
+   gistDiv = document.createElement('div');
+   gistDiv.setAttribute('class', 'js-gist-file-update-container js-task-list-container file-box');
+
+   gistBottomDiv = gistBottomDiv.appendChild(gistDiv);
+
+   gistDiv = document.createElement('div');
+   gistDiv.setAttribute('id', 'file-agist-txt');
+   gistDiv.setAttribute('class', 'file my-2');
+
+   gistBottomDiv = gistBottomDiv.appendChild(gistDiv);
+
+   gistDiv = document.createElement('div');
+   gistDiv.setAttribute('itemprop', 'text');
+   gistDiv.setAttribute('class', 'Box-body p-0 blob-wrapper data type-text');
+
+   gistBottomDiv = gistBottomDiv.appendChild(gistDiv);
+
+   let gistTable = document.createElement('table');
+   gistTable.setAttribute('data-tab-size', '8');
+   gistTable.setAttribute('data-paste-markdown-skip', '');
+   gistTable.setAttribute('class', 'highlight tab-size js-file-line-container');
+
+   gistBottomDiv.appendChild(gistTable);
+
+   let tr, trLast;
+   let td1, td2;
+
+   tr = document.createElement('tr');
+   td1 = document.createElement('td');
+   td2 = document.createElement('td');
+
+   td1.setAttribute('id', 'file-agist-txt-L1');
+   td1.setAttribute('class', 'blob-num js-line-number');
+   td1.setAttribute('data-line-number', '1');
+
+   td2.setAttribute('id', 'file-agist-txt-LC1');
+   td2.setAttribute('class', 'blob-code blob-code-inner js-file-line');
+   td2.appendChild(document.createTextNode('!!!!!! smiley'))
+
+   gistTable.appendChild(tr).appendChild(td1);
+   tr.appendChild(td2);
+   
+   return gistTopDiv;
+}
+
 //Add a gist to a HTML5 web page
 function addGistToPage(elem, url) {
    //HTTP request to retrieve the Gist
@@ -18,10 +78,11 @@ function addGistToPage(elem, url) {
          if(xhr.response.files) {
             //Send the content of the gist(s) to the console
             Object.keys(xhr.response.files).forEach(key => {
-               let elemGist = document.createElement('p');
-               elemGist.appendChild(document.createTextNode(xhr.response.files[key].content));
+               let elemGist = getGitHubGistElem(xhr.response.files[key]);
 
-               elem.parentNode.replaceChild(elemGist, elem);
+               if(elemGist) {
+                  elem.parentNode.replaceChild(elemGist, elem);
+               }
             });
          }
          else {
